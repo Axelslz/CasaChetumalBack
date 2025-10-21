75f8e612c32ff29f16b81c4919ca825cab813af9
Axelslz <axlsalazarr1602@gmail.com>
2025-10-21 13:31:25 -0600

    update file config

--- Back-Casachetumal/config/db.js
+++ Back-Casachetumal/config/db.js
@@ -1,15 +1,20 @@
 import { Sequelize } from 'sequelize';
 import 'dotenv/config';
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || 'basecasachetumal',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || '',
-  {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql'
+const dbUrl = process.env.DATABASE_URL;
+
+if (!dbUrl) {
+  throw new Error("Por favor define la variable de entorno DATABASE_URL dentro de tu archivo .env");
+}
+
+const sequelize = new Sequelize(dbUrl, {
+  dialect: 'postgres', 
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false 
+    }
   }
-);
+});
 
 export default sequelize;
-    
