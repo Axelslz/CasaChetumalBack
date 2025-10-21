3a6f7b7410ac6863bf86fa5f329b5a3acddd6c44
Axelslz <axlsalazarr1602@gmail.com>
2025-10-21 16:48:06 -0600

    update file server, configuration cors

--- Back-Casachetumal/server.js
+++ Back-Casachetumal/server.js
@@ -13,9 +13,11 @@ import { setupAssociations } from './models/relationships.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:5173';
+
 app.use(cors({
-  origin: 'http://localhost:5173', 
-  credentials: true 
+  origin: allowedOrigin,
+  credentials: true
 }));
 
 app.use(express.json());
