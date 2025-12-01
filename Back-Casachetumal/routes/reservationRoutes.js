8357b6956df85cf6a67260e04007abbcd78d5697
Axelslz <axlsalazarr1602@gmail.com>
2025-12-01 13:06:24 -0600

    update reservation

--- Back-Casachetumal/routes/reservationRoutes.js
+++ Back-Casachetumal/routes/reservationRoutes.js
@@ -10,7 +10,6 @@ router.post('/calculate-total', express.json(), calculateTotal);
 router.get('/occupied-dates', getOccupiedDates);
 router.post('/', upload.fields([ { name: 'idPhoto', maxCount: 1 }, { name: 'receipt', maxCount: 1 } ]), createReservation);
 router.get('/:id', getReservationById);
-
 router.get('/', authMiddleware, getReservations); 
 router.put('/:id/status', authMiddleware, express.json(), updateReservationStatus); 
 router.put('/:id/confirm-payment', authMiddleware, express.json(), confirmPayment); 
