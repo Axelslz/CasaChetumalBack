e43fac0555e3d339bf44cf89423c4396a60ca2cb
unknown <axlsalazarr1602@gmail.com>
2025-12-13 10:03:02 -0600

    update boot

--- Back-Casachetumal/config/db.js
+++ Back-Casachetumal/config/db.js
@@ -1,37 +1,37 @@
-import { Sequelize } from 'sequelize';
-import 'dotenv/config';
-
-const dbUrl = process.env.DATABASE_URL;
-
-if (!dbUrl) {
-  throw new Error("Por favor define la variable de entorno DATABASE_URL dentro de tu archivo .env");
-}
-
-const sequelize = new Sequelize(dbUrl, {
-  dialect: 'postgres', 
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false 
-    }
-  }
-});
-
-export default sequelize;
-
-
 // import { Sequelize } from 'sequelize';
 // import 'dotenv/config';
 
-//   const sequelize = new Sequelize(
+// const dbUrl = process.env.DATABASE_URL;
 
-//     process.env.DB_NAME || 'basecasachetumal',
-//     process.env.DB_USER || 'root',
-//     process.env.DB_PASSWORD || '',
-//   {
-//     host: process.env.DB_HOST || 'localhost',
-//     dialect: 'mysql'
+// if (!dbUrl) {
+//   throw new Error("Por favor define la variable de entorno DATABASE_URL dentro de tu archivo .env");
+// }
+
+// const sequelize = new Sequelize(dbUrl, {
+//   dialect: 'postgres', 
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false 
+//     }
 //   }
-// );
+// });
 
-// export default sequelize; 
+// export default sequelize;
+
+
+import { Sequelize } from 'sequelize';
+import 'dotenv/config';
+
+  const sequelize = new Sequelize(
+
+    process.env.DB_NAME || 'basecasachetumal',
+    process.env.DB_USER || 'root',
+    process.env.DB_PASSWORD || '',
+  {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql'
+  }
+);
+
+export default sequelize; 
